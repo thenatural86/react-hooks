35fbe60e6521f6d234b5d19ab80c5b93c27a3e52
thenatural86 <philip.smalls86@gmail.com>
2020-12-03 13:29:34 -0500

    onChange

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -6,9 +6,12 @@ import React, { useState } from 'react'
 // value, onChange
 
 const ControlledInputs = () => {
+  const [firstName, setFirstName] = useState('')
+  const [email, setEmail] = useState('')
+
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log('yolo world')
+    console.log(firstName, email)
   }
 
   return (
@@ -17,11 +20,23 @@ const ControlledInputs = () => {
         <form className='form' onSubmit={handleSubmit}>
           <div className='form-control'>
             <label htmlFor='firstName'>Name: </label>
-            <input type='text' name='firstName' id='firstName' />
+            <input
+              type='text'
+              name='firstName'
+              id='firstName'
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
           </div>
           <div className='form-control'>
             <label htmlFor='email'>Email: </label>
-            <input type='text' name='email' id='email' />
+            <input
+              type='text'
+              name='email'
+              id='email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
           <button type='submit'>add person</button>
         </form>
