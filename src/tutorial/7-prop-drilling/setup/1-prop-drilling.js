30b64a034f90d676d03666ccbef93b1b94b5ad82
thenatural86 <philip.smalls86@gmail.com>
2020-12-08 13:13:44 -0500

    pass function down into multiple components

--- src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
+++ src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
@@ -1,10 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
+import { data } from '../../../data'
 
 // more components
 // fix - context api, redux (for more complex cases)
 
 const PropDrilling = () => {
-  return <h2>prop drilling</h2>;
-};
+  const [people, setPeople] = useState(data)
 
-export default PropDrilling;
+  const removePerson = (id) => {
+    let newPeople = people.filter((person) => person.id !== id)
+    setPeople(newPeople)
+  }
+
+  return (
+    <section>
+      <h3>Prop Drillin'</h3>
+      <List people={people} removePerson={removePerson} />
+    </section>
+  )
+}
+
+const List = ({ people, removePerson }) => {
+  console.log(people)
+  return (
+    <>
+      {people.map((person) => {
+        return (
+          <SinglePerson
+            key={person.id}
+            {...person}
+            removePerson={removePerson}
+          />
+        )
+      })}
+    </>
+  )
+}
+
+const SinglePerson = ({ id, name, removePerson }) => {
+  return (
+    <div className='item'>
+      <p>{name}</p>
+      <button onClick={() => removePerson(id)}>remove</button>
+    </div>
+  )
+}
+
+export default PropDrilling
