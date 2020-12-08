7c1f6153a5258b7de832851934d32d8ff9644400
thenatural86 <philip.smalls86@gmail.com>
2020-12-08 13:54:12 -0500

    useContext demo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Setup from './tutorial/8-useContext/final/1-context-api'
+import Setup from './tutorial/8-useContext/setup/1-context-api'
 
 function App() {
   return (
