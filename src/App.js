2a94221741187d951efde719620081aa6e7694fb
thenatural86 <philip.smalls86@gmail.com>
2020-12-16 14:35:38 -0500

    usememo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Setup from './tutorial/11-react-router/setup/index'
+import Setup from './tutorial/12-memo-useMemo-useCallback/setup/index'
 
 function App() {
   return (
